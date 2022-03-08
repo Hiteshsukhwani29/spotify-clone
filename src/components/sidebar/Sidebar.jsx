75a13214c4b017abab01ffe032f173babc6dea85
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2022-03-08 23:13:42 +0530

    Add sidebar options

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import SpotifyLogo from '../../assets/logo_white';
 import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryIcon from '@material-ui/icons/LibraryMusic';
+import CreatePlaylistIcon from '@material-ui/icons/AddBox';
+import LikedIcon from '@material-ui/icons/Favorite';
 
 function Sidebar() {
   return (
     <div className='min-h-screen flex-[0.2] bg-[#000000] min-w-[230px] text-white px-3'>
         <SpotifyLogo />
-        <SidebarOption Text="Home"/>
-        <SidebarOption Text="Search"/>
-        <SidebarOption Text="Your library"/>
+        <SidebarOption Icon={HomeIcon} Text="Home"/>
+        <SidebarOption Icon={SearchIcon} Text="Search"/>
+        <SidebarOption Icon={LibraryIcon} Text="Your library"/>
+        <div className='h-6'></div>        
+        <SidebarOption Icon={CreatePlaylistIcon} Text="Create Playlist"/>
+        <SidebarOption Icon={LikedIcon} Text="Liked Songs"/>
+        <div className='h-[0.07px] mx-3 mt-2 bg-[#282828] mb-3'></div>
+        <SidebarOption Text="Jazz"/>
+        <SidebarOption Text="Hip Hop"/>
+        <SidebarOption Text="Mega Punjabi Hits"/>
     </div>
   )
 }
--- src/components/sidebar/SidebarOption.jsx
+++ src/components/sidebar/SidebarOption.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function SidebarOption({Icon, Text}) {
+function SidebarOption({ Icon, Text }) {
   return (
     <div>
-        <div className='text-gray'>
-            {Text}
-        </div>
+      <div className='text-gray hover:text-white transition-colors h-10 cursor-pointer flex align-middle'>
+        {Icon && <Icon className='mx-3 text-gray' />}
+        {Icon ? <h1>{Text}</h1> : <p className='mx-3 text-sm'>{Text}</p>}
+      </div>
     </div>
   )
 }
